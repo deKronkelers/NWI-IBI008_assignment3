fbca34fffbe91e193a6eb7adccccf3f7fa1b4d3a
Hendrikto <hendrik.to@gmail.com>
2016-10-05 14:34:26 +0200

    Extract attributes.

--- answers/assignment3_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment3_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -5,8 +5,9 @@ import scipy.io
 
 # assignment 3.1.1
 wine_data = scipy.io.loadmat("./data/wine.mat")
-attribute_names = [nl[0] for nl in wine_data["attributeNames"][0]]
 
+attributes = [wine_data["X"][:, i] for i in range(wine_data["X"].shape[1])]
+attribute_names = [nl[0] for nl in wine_data["attributeNames"][0]]
 attribute_units = [
     "g/dm^3", "g/dm^3", "g/dm^3", "g/dm^3", "g/dm^3", "mg/dm^3", "mg/dm^3",
     "g/cm^3", "pH", "g/dm^3", "% vol."
