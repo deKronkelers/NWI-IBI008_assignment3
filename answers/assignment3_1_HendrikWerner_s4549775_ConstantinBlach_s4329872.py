91078892af7af3f3ec77fca5c883d9d084689eb4
Hendrikto <hendrik.to@gmail.com>
2016-10-05 17:04:54 +0200

    Add answer to 3.1.4.

--- answers/assignment3_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment3_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -28,3 +28,12 @@ tree.export_graphviz(decision_tree, out_file="tree.dot", class_names=class_names
 # assignment 3.1.3
 test_wine = array([6.9 , 1.09, 0.06, 2.1, 0.0061, 12, 31, 0.99, 3.5, 0.64, 12])
 print("The test wine is probably a {} wine.".format(("red", "white")[decision_tree.predict(test_wine.reshape(1, -1))[0]]))
+
+# assignment 3.1.4
+successful_classifications = 0
+for i, wine in enumerate(X):
+    if decision_tree.predict(wine.reshape(1, -1)) == classes[i]:
+        successful_classifications += 1
+print("The fitted decision tree classifies {}% of the learning data correctly.".format(
+    successful_classifications * 100 / X.shape[0]
+))
