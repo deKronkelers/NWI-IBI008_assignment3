cc9922f35cc3c1fa02b9848825f021ae8f94dc74
Hendrikto <hendrik.to@gmail.com>
2016-10-05 16:09:22 +0200

    Add answer to 3.1.2.

--- answers/assignment3_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment3_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -2,6 +2,7 @@
 # author Constantin Blach s4329872
 
 import scipy.io
+from sklearn import tree
 
 # assignment 3.1.1
 wine_data = scipy.io.loadmat("./data/wine.mat")
@@ -18,3 +19,7 @@ classes = wine_data["y"]
 class_names = [wine_data["classNames"][i, 0][0] for i in range(2)]
 
 # assignment 3.1.2
+decision_tree = tree.DecisionTreeClassifier(min_samples_split=100)
+decision_tree = decision_tree.fit(X, classes)
+
+tree.export_graphviz(decision_tree, out_file="tree.dot", class_names=class_names)
