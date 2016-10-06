ef53232153494adb81bb31e60b1e9974956a2ab1
Hendrikto <hendrik.to@gmail.com>
2016-10-06 19:45:21 +0200

    Add answer to 3.2.1.

--- answers/assignment3_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment3_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -1,4 +1,26 @@
 # author: Hendrik Werner s4549775
 # author Constantin Blach s4329872
 
-# assignment 3.2
+import scipy.io
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn import tree
+
+# assignment 3.2.1
+wine_data = scipy.io.loadmat("./data/wine.mat")
+X_train, X_test, classes_train, classes_test = train_test_split(
+    wine_data["X"], wine_data["y"]
+)
+
+depths = range(2, 21)
+errors = []
+for depth in depths:
+    decision_tree = tree.DecisionTreeClassifier(max_depth=depth)
+    decision_tree.fit(X_train, classes_train)
+    errors.append(1 - decision_tree.score(X_test, classes_test))
+
+f = plt.subplot(111)
+f.plot(depths, errors)
+f.set_xlabel("decision tree max depth")
+f.set_ylabel("classification error")
+plt.show()
