d242855209126e4441bf5668fbe0cc3e91aee93f
Hendrikto <hendrik.to@gmail.com>
2016-10-09 03:30:39 +0200

    Apply DRY principle.

--- answers/assignment3_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment3_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -26,11 +26,20 @@ def classification_errors(X_train, y_train, X_test, y_test, depths):
 
 depths = range(2, 21)
 errors = classification_errors(X_train, y_train, X_test, y_test, depths)
-f = plt.subplot(111)
-f.plot(depths, errors)
-f.set_xlabel("decision tree max depth")
-f.set_ylabel("classification error")
-plt.show()
+
+
+def plot_error(
+        depths, errors,
+        xlabel="decision tree max depth",
+        ylabel="classification error"
+):
+    f = plt.subplot(111)
+    f.plot(depths, errors)
+    f.set_xlabel(xlabel)
+    f.set_ylabel(ylabel)
+    plt.show()
+
+plot_error(depths, errors)
 
 # assignment 3.2.2
 kf = KFold(n_splits=10, shuffle=True)
@@ -47,8 +56,4 @@ for train_index, test_index in kf.split(X):
 errors_list = array(errors_list)
 average_errors = [col.mean() for col in (errors_list[:, i] for i in range(errors_list.shape[1]))]
 
-f = plt.subplot(111)
-f.plot(depths, average_errors)
-f.set_xlabel("decision tree max depth")
-f.set_ylabel("average classification error")
-plt.show()
+plot_error(depths, average_errors, ylabel="average classification error")
