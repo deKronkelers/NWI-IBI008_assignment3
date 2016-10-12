bf97e6ede405515328b7e525f078758aa2db67cc
Hendrikto <hendrik.to@gmail.com>
2016-10-12 19:04:34 +0200

    Also plot classification error on the training set.

--- answers/assignment3_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assignment3_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -10,22 +10,25 @@ from sklearn.model_selection import train_test_split, KFold
 
 # Calculate classification errors for different tree depths.
 def classification_errors(X_train, y_train, X_test, y_test, depths):
-    errors = []
+    errors_test = []
+    errors_train = []
     for depth in depths:
         decision_tree = tree.DecisionTreeClassifier(max_depth=depth)
         decision_tree.fit(X_train, y_train)
-        errors.append(1 - decision_tree.score(X_test, y_test))
-    return errors
+        errors_test.append(1 - decision_tree.score(X_test, y_test))
+        errors_train.append(1 - decision_tree.score(X_train, y_train))
+    return errors_test, errors_train
 
 
 # Plot the tree depth against the classification error.
-def plot_error(
-        depths, errors,
+def plot_errors(
+        depths, errors_list,
         xlabel="decision tree max depth",
         ylabel="classification error"
 ):
     f = plt.subplot(111)
-    f.plot(depths, errors)
+    for errors in errors_list:
+        f.plot(depths, errors)
     f.set_xlabel(xlabel)
     f.set_ylabel(ylabel)
     plt.show()
@@ -38,21 +41,28 @@ y = wine_data["y"]
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 depths = range(2, 21)
 errors = classification_errors(X_train, y_train, X_test, y_test, depths)
-plot_error(depths, errors)
+plot_errors(depths, errors)
 
 # assignment 3.2.2
 kf = KFold(n_splits=10, shuffle=True)
-errors_list = []
+errors_list_test = []
+errors_list_train = []
 for train_index, test_index in kf.split(X):
-    errors_list.append(classification_errors(
+    errors_test, errors_train = classification_errors(
         X[train_index],
         y[train_index],
         X[test_index],
         y[test_index],
         depths
-    ))
+    )
+    errors_list_test.append(errors_test)
+    errors_list_train.append(errors_train)
 
-errors_list = array(errors_list)
-average_errors = [col.mean() for col in (errors_list[:, i] for i in range(errors_list.shape[1]))]
 
-plot_error(depths, average_errors, ylabel="average classification error")
+def average_errors(errors_list):
+    errors_list = array(errors_list)
+    return [col.mean() for col in (errors_list[:, i] for i in range(errors_list.shape[1]))]
+
+
+average_errors = (average_errors(errors_list_test), average_errors(errors_list_train))
+plot_errors(depths, average_errors, ylabel="average classification error")
